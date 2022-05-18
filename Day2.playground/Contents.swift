4666f6e7f47ae81a7fb248f8c403fc82db4fa171
Ibroad <57664875+Ibroad@users.noreply.github.com>
2022-05-18 23:39:04 +0800

    Day2

---
+++ Day2.playground/Contents.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+// Booleans
+
+
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+let hasSuffix = filename.hasSuffix(".jpg")
+print(hasSuffix)
+
+print(120.isMultiple(of: 3))
+var mutiple = 120.isMultiple(of: 3)
+
+print(mutiple)
+print(!mutiple)
+
+mutiple.toggle()
+print(mutiple)
+
+
+// Join strings together
+
+let nameA = "Bob"
+let nameB = "Aryn"
+let ageA = 33
+let ageB = 44
+
+print("Hello "+nameA)
+print("Hello \(nameB)")
+print("What's your age, Bob")
+print("\(nameA): I'm \(ageA)")
+print(nameB+" "+"I'm "+String(ageB))
+print("Oh, the sum of our age is \(ageA+ageB)")
+
+
+// Checkpoint 1
+//Shift+Option+8 -> °
+
+
+let celsius = 20
+let fahreheit = celsius*9/5+32
+print("Temperature(°C) is \(celsius)°C\nTemperature(°F) is \(fahreheit)°F")
