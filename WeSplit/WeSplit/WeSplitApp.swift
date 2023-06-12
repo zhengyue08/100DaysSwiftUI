c8e05f98a69dba876ac8f70daf8c2d4eee21e003
Zheng Yue <57664875+Ibroad@users.noreply.github.com>
2023-06-12 15:56:46 +0800

    Project 1: WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yue ZHENG on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
