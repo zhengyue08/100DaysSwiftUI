35132688c46891fed3adfb022003250ffaaa1f02
Zheng Yue <57664875+Ibroad@users.noreply.github.com>
2023-06-16 00:34:20 +0800

    Project2: Guess the flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Yue ZHENG on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
