c5bc2af0afc08fdf0d62ced62957bfdcd3bd325c
Zheng Yue <57664875+Ibroad@users.noreply.github.com>
2023-06-25 22:41:09 +0800

    Day28: project 4 challenge

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -19,49 +19,12 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
-    static var defaultWakeUp: Date {
-        var components = DateComponents()
-        components.hour = 7
-        components.minute = 0
-        return Calendar.current.date(from: components) ?? Date.now
-    }
-    
-    var body: some View {
-        NavigationStack{
-            Form {
-                
-                VStack(alignment: .leading) {
-                    Text("When do you want to wake up").font(.headline)
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("Desired amount of sleep").font(.headline)
-                    Stepper("\(sleepAmount.formatted())  hours", value: $sleepAmount, in: 4...12, step: 0.5)
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("Daily Coffe intake").font(.headline)
-                    Stepper((coffeAmount == 1) ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 0...20)
-                }
-               
-            }
-            .navigationTitle("Better Rest")
-            .toolbar {
-                Button("Calculate") {
-                    calculateBedtime()
-                }
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK"){}
-            } message: {
-                Text(alertMessage)
-            }
-        }
+    let coffeIntakeCups = 0...20
 
-    }
+    //challenge
     
-    func calculateBedtime() {
+    
+    var sleepTime: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -73,19 +36,102 @@ struct ContentView: View {
             
             let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: Double(sleepAmount), coffee: Double(coffeAmount))
             
-            let sleepTime = wakeUp - prediction.actualSleep
+            return (wakeUp - prediction.actualSleep).formatted(date: .omitted, time: .shortened)
             
-            alertTitle = "Your bedtime is ..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
             
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Oops, something goes wrong when calculating your bedtime"
+            return Date.now.formatted(date: .omitted, time: .shortened)
         }
         
-        showingAlert = true
-        
     }
+    
+    
+    
+    static var defaultWakeUp: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Form {
+                
+                Section {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+                } header: {
+                    Text("When do you want to wake up").font(.headline)
+                }
+                
+                Section {
+                    Stepper("\(sleepAmount.formatted())  hours", value: $sleepAmount, in: 4...12, step: 0.5)
+                } header: {
+                    Text("Desired amount of sleep").font(.headline)
+                }
+                
+                Section {
+//                    Stepper((coffeAmount == 1) ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 0...20)
+                    
+                    Picker("Daily Coffe intake", selection: $coffeAmount) {
+                        ForEach(coffeIntakeCups, id: \.self) { cup in
+                            Text((cup == 1 || cup == 0) ? "\(cup) cup" : "\(cup) cups")
+                        }
+                    }
+                    
+                } header: {
+                    Text("Daily Coffe intake").font(.headline)
+                }
+                
+                //challenge
+                Section{
+                    Text(sleepTime).font(.largeTitle).frame(maxWidth: .infinity, alignment: .center)
+                } header: {
+                    Text("ideal Bedtime")
+                }
+               
+            }
+            .navigationTitle("Better Rest")
+//            .toolbar {
+//                Button("Calculate") {
+//                    calculateBedtime()
+//                }
+//            }
+//            .alert(alertTitle, isPresented: $showingAlert) {
+//                Button("OK"){}
+//            } message: {
+//                Text(alertMessage)
+//            }
+            
+        }
+
+    }
+    
+//    func calculateBedtime() {
+//        do {
+//            let config = MLModelConfiguration()
+//            let model = try SleepCalculator(configuration: config)
+//
+//            //More code to come here
+//            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+//            let hour = (components.hour ?? 0) * 60 * 60
+//            let minute = (components.minute ?? 0) * 60
+//
+//            let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: Double(sleepAmount), coffee: Double(coffeAmount))
+//
+//            let sleepTime = wakeUp - prediction.actualSleep
+//
+//            alertTitle = "Your bedtime is ..."
+//            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+//
+//        } catch {
+//            alertTitle = "Error"
+//            alertMessage = "Oops, something goes wrong when calculating your bedtime"
+//        }
+//
+//        showingAlert = true
+//
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
